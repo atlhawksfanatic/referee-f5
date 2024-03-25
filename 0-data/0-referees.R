7abe0a07c807c5e7c399b4631db4c205887f87c8
atlhawksfanatic <atlhawksfanatic@gmail.com>
2024-03-25 00:52:07 +0000

    local file for referee box stats creation

--- 0-data/0-referees.R
+++ 0-data/0-referees.R
@@ -92,36 +92,36 @@ dbGetQuery(duck_con, "DESCRIBE referee_assignments")
 
 # ---- ref-box ------------------------------------------------------------
 
-# insert in the referees from the box score
-ref_box_url <- str_glue("https://raw.githubusercontent.com/",
-                        "atlhawksfanatic/L2M/master/0-data/",
-                        "stats_nba/stats_nba_box.csv")
+# insert in the referees from the box score, this might be available local
+lcl_ref_box <- "0-data/duckdb/stats_nba/ref_box.csv"
+if (file.exists(lcl_ref_box)) {
+  ref_box <- read_csv(lcl_ref_box) |> 
+    # Hack for 0011600107
+    filter(game_id != "0011600107")
+  
+  ref_vars <- sapply(ref_box, class) |> 
+    enframe() |> 
+    mutate(duck_type = duck_types_cross[value])
+  
+  # Make the duckdb sql code for variable types
+  ref_duck_vars <- glue::glue_collapse(
+    glue::glue("{ref_vars$name} {ref_vars$duck_type}"),
+    ", ")
+  
+  create_ref <- glue::glue("DROP TABLE IF EXISTS ref_box;
+                         CREATE TABLE ref_box (",
+                           "{ref_duck_vars}, ",
+                           "PRIMARY KEY(game_id))")
+  
+  dbSendQuery(duck_con, create_ref)
+  
+  # Write data to table
+  dbWriteTable(duck_con, "ref_box", ref_box,
+               append = TRUE, row.names = FALSE)
+  
+  dbGetQuery(duck_con, "SELECT COUNT(*) FROM ref_box")
+  dbGetQuery(duck_con, "DESCRIBE ref_box")
+}
 
-ref_assign <- read_csv(ref_assign_url) |> 
-  select(-season, -game_id) |> 
-  mutate(game_date = as.Date(game_date, format = "%m/%d/%Y"))
-
-ref_vars <- sapply(ref_assign, class) |> 
-  enframe() |> 
-  mutate(duck_type = duck_types_cross[value])
-
-# Make the duckdb sql code for variable types
-ref_duck_vars <- glue::glue_collapse(
-  glue::glue("{ref_vars$name} {ref_vars$duck_type}"),
-  ", ")
-
-create_ref <- glue::glue("DROP TABLE IF EXISTS referee_assignments;
-                         CREATE TABLE referee_assignments (",
-                         "{ref_duck_vars}, ",
-                         "PRIMARY KEY(game_code))")
-
-dbSendQuery(duck_con, create_ref)
-
-# Write data to table
-dbWriteTable(duck_con, "referee_assignments", ref_assign,
-             append = TRUE, row.names = FALSE)
-
-dbGetQuery(duck_con, "SELECT COUNT(*) FROM referee_assignments")
-dbGetQuery(duck_con, "DESCRIBE referee_assignments")
 
 
