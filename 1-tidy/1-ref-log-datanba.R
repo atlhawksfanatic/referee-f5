fc0fe7ae6364072fd7d3b3831726b45e1a1f1f5e
robert <atlhawksfanatic@gmail.com>
2024-03-30 12:36:30 -0400

    vanduyne/van duyne and sanity checks

--- 1-tidy/1-ref-log-datanba.R
+++ 1-tidy/1-ref-log-datanba.R
@@ -31,6 +31,9 @@ violation_regex <- "\\(([[:print:]]+)\\)"
 
 dbListTables(duck_con)
 
+# dbSendQuery(duck_con, "drop table ref_fouls_datanba")
+# dbSendQuery(duck_con, "drop table ref_violations_datanba")
+
 szns <- tbl(duck_con, "datanba") |> 
   left_join(tbl(duck_con, "game_ids"), by = c("game_id" = "gid")) |> 
   select(season) |> 
@@ -58,11 +61,15 @@ szns |>
         maybe_ref = str_extract(de, foul_regex, group = 2),
         reffy = str_extract(de, violation_regex, group = 1)
       ) |> 
-      mutate(ref = case_when(grepl("Drell", maybe_ref) ~ "I Hwang",
+      mutate(ref = case_when(maybe_ref == "Casey" ~ "G Petraitis",
+                             grepl("Drell", maybe_ref) ~ "I Hwang",
                              grepl("[0-9]", maybe_ref) ~ NA_character_,
                              is.na(maybe_ref) ~ reffy,
                              T ~ maybe_ref),
-             ref_match = toupper(ref))
+             ref_match = ref |> 
+               # Remove the second space if it exists
+               str_replace("^(\\S+\\s+\\S+)\\s+", "\\1") |> 
+               str_to_upper())
     
     foul_crosswalk <- identify_fouls |> 
       select(game_id, official_1, official_2, official_3, official_4) |> 
@@ -73,8 +80,9 @@ szns |>
       filter(!is.na(official)) |> 
       mutate(ref_match = str_replace(official,
                                      "([A-Z]?)([[:graph:]]*) (.*)",
-                                     "\\1  \\3") |> 
-               str_replace("\\s", "") |> 
+                                     "\\1 \\3") |> 
+               str_replace("^(\\S+\\s+\\S+)\\s+", "\\1") |> 
+               # str_replace("\\s", "") |> 
                str_to_upper())
     
     ref_fouls <- identify_fouls |> 
@@ -82,7 +90,7 @@ szns |>
       select(game_id, evt,
              etype, etype_desc,
              mtype, mtype_desc,
-             position, official)
+             position, official, ref_match, de)
     
     foul_vars <- sapply(ref_fouls, class) |> 
       enframe() |> 
@@ -131,10 +139,14 @@ szns |>
       mutate(
         maybe_ref = str_extract(de, violation_regex, group = 1)
       ) |> 
-      mutate(ref = case_when(grepl("Drell", maybe_ref) ~ "I Hwang",
+      mutate(ref = case_when(maybe_ref == "J VanDuyne" ~ "J Van Duyne",
+                             maybe_ref == "Casey" ~ "G Petraitis",
+                             grepl("Drell", maybe_ref) ~ "I Hwang",
                              grepl("[0-9]", maybe_ref) ~ NA_character_,
                              T ~ maybe_ref),
-             ref_match = toupper(ref))
+             ref_match = ref |> 
+               str_replace("^(\\S+\\s+\\S+)\\s+", "\\1") |> 
+               str_to_upper())
     
     violation_crosswalk <- identify_violation |> 
       select(game_id, official_1, official_2, official_3, official_4) |> 
@@ -145,8 +157,9 @@ szns |>
       filter(!is.na(official)) |> 
       mutate(ref_match = str_replace(official,
                                      "([A-Z]?)([[:graph:]]*) (.*)",
-                                     "\\1  \\3") |> 
-               str_replace("\\s", "") |> 
+                                     "\\1 \\3") |> 
+               str_replace("^(\\S+\\s+\\S+)\\s+", "\\1") |> 
+               # str_replace("\\s", "") |> 
                str_to_upper())
     
     ref_violations <- identify_violation |> 
@@ -154,7 +167,7 @@ szns |>
       select(game_id, evt,
              etype, etype_desc,
              mtype, mtype_desc,
-             position, official)
+             position, official, ref_match, de)
     
     violation_vars <- sapply(ref_violations, class) |> 
       enframe() |> 
@@ -179,3 +192,26 @@ szns |>
 
 dbGetQuery(duck_con, "SELECT COUNT(*) FROM ref_violations_datanba")
 dbGetQuery(duck_con, "DESCRIBE ref_violations_datanba")
+
+# ---- sanity -------------------------------------------------------------
+
+na_officialz <- tbl(duck_con, "ref_fouls_datanba") |> 
+  union_all(tbl(duck_con, "ref_violations_datanba")) |> 
+  filter(is.na(official)) |> 
+  collect()
+
+na_officialz |> 
+  group_by(ref_match) |> 
+  tally() |> 
+  arrange(n)
+
+na_officialz |> 
+  filter(is.na(ref_match))
+
+na_officialz |> 
+  filter(ref_match %in% c("T MADDOX", "B TAYLOR", "T BROTHERS")) |> 
+  View()
+
+tbl(duck_con, "ref_box") |> 
+  filter(game_id == "0042000133") |> 
+  glimpse()
